6aacced581a0d755eb2fb6dc161fce887aa8e0d8
ygona <137731443+ygona@users.noreply.github.com>
2023-07-27 18:10:20 -0400

    Initial commit

---
+++ src/main/java/com/example/surveyForm/SurveyFormApplication.java
@@ -0,0 +1,13 @@
+package com.example.surveyForm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveyFormApplication.class, args);
+	}
+
+}
